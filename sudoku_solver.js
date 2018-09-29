8950ffdc76c7fe603e6a2d44ca2de7b8520e2049
asolanky <alwinsolanky@gmail.com>
2018-09-29 23:18:36 +0100

    Second passing test

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -8,3 +8,21 @@ module.exports.parseBoard = function(board) {
     });
   });
 };
+
+module.exports.saveEmptyPositions = function(board) {
+  // Create an array to save the positions
+  var emptyPositions = [];
+
+  // Check every square in the puzzle for a zero
+  for(var i = 0; i < board.length; i++) {
+    for(var j = 0; j < board[i].length; j++) {
+      // If a zero is found, save that position
+      if(board[i][j] === 0) {
+        emptyPositions.push([i, j]);
+      }
+    }
+  }
+
+  // Return the positions
+  return emptyPositions;
+};

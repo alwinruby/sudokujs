1bf9eb4323ed0d1a0e5b30c0f1238483c8357a86
asolanky <alwinsolanky@gmail.com>
2018-09-29 22:44:06 +0100

    First Passing test

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -1,3 +1,10 @@
 module.exports.parseBoard = function(board) {
-
+  // split the board at each new line, and use map
+  // to split each row into an array of characters
+  return board.split('\n').map(function(row) {
+    // use map to convert the characters into integers
+    return row.split('').map(function(num) {
+      return +num;
+    });
+  });
 };
